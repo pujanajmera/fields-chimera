4b0542ce8a2eb75e769d79cfe4a178c3218e3643
Pujan Ajmera <ajmerapujan@gmail.com>
2024-03-08 15:21:47 -0800

    Update fields-chimera.py

--- fields-chimera.py
+++ fields-chimera.py
@@ -44,7 +44,7 @@ def process_data(file_path):
                 reading_basis_matrix = True
             elif reading_basis_matrix and line.startswith('#'):
                 try:
-                    basis_matrix.append([float(x) for x in line.split()[1:4]])
+                    basis_matrix.append([float(x) for x in line[1:].split()[0:3]])
                 except:
                     raise ValueError(f"Basis matrix line not formatted correctly; matrix list of length {len(basis_matrix)}")
                 if len(basis_matrix) == 3:
@@ -62,6 +62,7 @@ def process_data(file_path):
         warnings.warn("Center or basis matrix not found, no transformation will be made")
     if sample_density:
         check_field(np.array(field), np.array(sample_density))
+    print(np.array(basis_matrix))
     return np.array(sample_density), np.array(volume_box), np.array(center), np.array(basis_matrix), np.array(field)
 
 def check_field(field_array, sample_density_array):
@@ -91,8 +92,9 @@ def transform_field(field_array, center_array, basis_matrix_array):
     '''
     field_coords = field_array[:, 0:3]
     field_vecs = field_array[:, 3:6]
+    print(field_coords, field_vecs, basis_matrix_array)
     # Need to transform and recenter field coords, but only transform field vecs
-    transformed_field_coords = np.matmul(field_coords, basis_matrix_array.T)
+    transformed_field_coords = field_coords@basis_matrix_array.T
     transformed_field_coords = transformed_field_coords + center_array
     transformed_field_vecs = np.matmul(field_vecs, basis_matrix_array.T)
     transformed_field_array = np.concatenate((transformed_field_coords, transformed_field_vecs), axis=1)
@@ -102,14 +104,14 @@ def generate_tip_tail_vectors(field_array, sample_density_array, volume_box_arra
     '''
     This function generates the tip and tail vectors for the field
     Inputs:
-        field_array: Field array (transformed or not)
-        sample_density_array: Sample density array
+        field_array: Field array (transformed or not) Shape (N, 6)
+        sample_density_array: Sample density array Shape (3,)
         volume_box_array: Volume box array
     Outputs:
         tip_to_tail_vectors: Tip to tail vectors
     '''
     if sample_density_array!=[] and volume_box_array!=[]:
-        max_vector_length = 0.5*np.min(volume_box_array/sample_density_array)
+        max_vector_length = 0.9*np.min(volume_box_array/sample_density_array)
     else:
         max_vector_length = 0.25
 
@@ -118,17 +120,23 @@ def generate_tip_tail_vectors(field_array, sample_density_array, volume_box_arra
     tail_vecs = field_array[:, 0:3] - field_array[:, 3:6]/2
     tip_vecs = field_array[:, 0:3] + field_array[:, 3:6]/2
 
-    tip_to_tail_vectors = np.concatenate((tip_vecs, tail_vecs), axis=1)
+    tip_to_tail_vectors = np.concatenate((tail_vecs, tip_vecs), axis=1)
 
     return tip_to_tail_vectors
 
-def generate_bild_file(tip_to_tail_vectors, transformed_field_array, chimera_type):
+def generate_bild_file(tip_to_tail_vectors, 
+                       transformed_field_array, 
+                       chimera_type, 
+                       percentile, 
+                       sparsify_factor):
     '''
     This function generates the BILD file for the 3D vector field
     Inputs:
         tip_to_tail_vectors: Tip to tail vectors
         transformed_field_array: Field array
         chimera_type: Chimera type
+        percentile: Percentile cutoff for sparsifying the field
+        sparsify_factor: Sparsification factor for the field
     Outputs:
         None
     '''
@@ -137,15 +145,18 @@ def generate_bild_file(tip_to_tail_vectors, transformed_field_array, chimera_typ
     field_mags_max = np.max(field_mags)
     field_mags_min = np.min(field_mags)
     field_mags = (field_mags-field_mags_min)/field_mags_max
-    r = field_mags
-    g = field_mags
-    b = np.ones((field_mags.shape[0], 1))
-
+    percentile_cutoff = np.percentile(field_mags, percentile)
+    r = (1-field_mags)[::sparsify_factor]
+    g = (1-field_mags)[::sparsify_factor]
+    b = (np.ones((field_mags.shape[0], 1))).astype(int)[::sparsify_factor]
+    field_mags = field_mags[::sparsify_factor]
+    tip_to_tail_vectors = tip_to_tail_vectors[::sparsify_factor]
     with open('output.bild', 'w') as bild:
         bild.write(".transparency 0.25\n")
         for i in range(tip_to_tail_vectors.shape[0]):
-            bild.write(f".color {r[i]} {g[i]} {b[i][0]}\n")
-            bild.write(f".arrow {tip_to_tail_vectors[i, 0]} {tip_to_tail_vectors[i, 1]} {tip_to_tail_vectors[i, 2]} {tip_to_tail_vectors[i, 3]} {tip_to_tail_vectors[i, 4]} {tip_to_tail_vectors[i, 5]} 0.01 0.02 0.5\n")
+            if field_mags[i] > percentile_cutoff:
+                bild.write(f".color {r[i]} {g[i]} {b[i][0]}\n")
+                bild.write(f".arrow {tip_to_tail_vectors[i, 0]} {tip_to_tail_vectors[i, 1]} {tip_to_tail_vectors[i, 2]} {tip_to_tail_vectors[i, 3]} {tip_to_tail_vectors[i, 4]} {tip_to_tail_vectors[i, 5]} 0.01 0.02 0.5\n")
     bild.close()
 
 def main():
@@ -154,6 +165,8 @@ def main():
     parser.add_argument('-p', metavar='protein_file_path', type=str, help="Input protein PDB file")
     parser.add_argument('-o', metavar='output_file_name', type=str, help="Output file name (will end in .py by default, so don't include an extension)", default='output')
     parser.add_argument('-ch', metavar='chimera_type', type=str, help="Output file type (either 'chimera' or 'chimerax'); Default: chimerax", default='chimerax')
+    parser.add_argument('-c', metavar='percentile', type=int, help="Top Nth percentile for sparsifying the field; Default: 0", default=0)
+    parser.add_argument('-s', metavar='sparsify_factor', type=int, help="Sparsification factor for the field (can be used in tandem with percentile_cutoff); Default: 1", default=1)
 
     args = parser.parse_args()
     sample_density_array, volume_box_array, center_array, basis_matrix_array, field_array = process_data(args.v)
@@ -165,6 +178,6 @@ def main():
     
     tip_to_tail_vectors = generate_tip_tail_vectors(transformed_field_array.copy(), sample_density_array, volume_box_array)
 
-    generate_bild_file(tip_to_tail_vectors, transformed_field_array, args.ch)
+    generate_bild_file(tip_to_tail_vectors, transformed_field_array, args.ch, args.c, args.s)
 
 main()
